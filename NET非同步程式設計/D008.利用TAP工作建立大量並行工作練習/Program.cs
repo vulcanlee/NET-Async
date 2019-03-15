0dab373a1f3a1a329522c50e7b2aff04c46af04f
李進興 Vulcan <vulcan@miniasp.com>
2019-03-15 14:23:43 +0800

    D009.主執行緒與背景執行緒特性

---
+++ NET非同步程式設計/D008.利用TAP工作建立大量並行工作練習/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D008.利用TAP工作建立大量並行工作練習
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ NET非同步程式設計/D009.主執行緒與背景執行緒特性/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace D009.主執行緒與背景執行緒特性
+{
+    class Program
+    {
+        // Thread 使用的方法
+        public static void Main()
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < 300; i++) { Console.Write("F"); }
+            })
+            { IsBackground = true }.Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 300; i++) { Console.Write("B"); }
+            })
+            { IsBackground = false }.Start();
+
+            ThreadPool.QueueUserWorkItem((x) =>
+            {
+                for (int i = 0; i < 300; i++) { Console.Write("P"); }
+            });
+
+            for (int i = 0; i < 100; i++) { Console.Write("M"); }
+
+            Console.WriteLine("請按任一鍵，以結束執行");
+            Console.ReadKey();
+        }
+    }
+}
