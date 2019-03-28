50a51db78b8b20c0a25995fa605a3be39b0f435a
Vulcan Lee <vulcan.lee@gmail.com>
2019-03-28 14:09:19 +0800

    .

--- NET非同步程式設計/D008.利用TAP工作建立大量並行工作練習/Program.cs
+++ NET非同步程式設計/D008.利用TAP工作建立大量並行工作練習/Program.cs
@@ -17,18 +17,19 @@ namespace D008.利用TAP工作建立大量並行工作練習
 
             for (int i = 0; i < 10; i++)
             {
+                var idx = i;
                 Task.Run(() =>
                 {
                     HttpClient client = new HttpClient();
-                    Console.WriteLine($"第 {i}-1 測試開始時間 {DateTime.Now}");
+                    Console.WriteLine($"第 {idx}-1 測試開始時間 {DateTime.Now}");
                     var result = client.GetStringAsync(url).Result;
-                    Console.WriteLine($"第 {i}-1 測試結果內容 {result}");
-                    Console.WriteLine($"第 {i}-1 測試結束時間 {DateTime.Now}");
+                    Console.WriteLine($"第 {idx}-1 測試結果內容 {result}");
+                    Console.WriteLine($"第 {idx}-1 測試結束時間 {DateTime.Now}");
 
-                    Console.WriteLine($"第 {i}-2 測試開始時間 {DateTime.Now}");
+                    Console.WriteLine($"第 {idx}-2 測試開始時間 {DateTime.Now}");
                     result = client.GetStringAsync(url).Result;
-                    Console.WriteLine($"第 {i}-2 測試結果內容 {result}");
-                    Console.WriteLine($"第 {i}-2 測試結束時間 {DateTime.Now}");
+                    Console.WriteLine($"第 {idx}-2 測試結果內容 {result}");
+                    Console.WriteLine($"第 {idx}-2 測試結束時間 {DateTime.Now}");
                 });
             }
 
