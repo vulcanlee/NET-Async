63dccff07b47ae2acb7cf7e2a3e892765c42b7e6
李進興 Vulcan <vulcan@miniasp.com>
2019-03-28 11:00:01 +0800

    C009.執行緒不安全的範例

--- NET非同步程式設計/C007.使用執行緒做到非同步處理作業/Program.cs
+++ NET非同步程式設計/C007.使用執行緒做到非同步處理作業/Program.cs
@@ -14,35 +14,37 @@ namespace C007.使用執行緒做到非同步處理作業
             Console.WriteLine($"建立兩個執行緒物件");
             Thread thread1 = new Thread(() =>
             {
-                Console.WriteLine($"執行緒1 的 ID={Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"執行緒1 的 ID={Thread.CurrentThread.ManagedThreadId} ( 輸出 X )");
                 Thread.Sleep(900);
                 for (int i = 0; i < 20; i++)
                 {
                     Thread.Sleep(100); Console.Write("X");
                 }
-                Console.WriteLine();
+                //Console.WriteLine();
             });
             Thread thread2 = new Thread(() =>
             {
-                Console.WriteLine($"執行緒2 的 ID={Thread.CurrentThread.ManagedThreadId}");
+                Console.WriteLine($"執行緒2 的 ID={Thread.CurrentThread.ManagedThreadId} ( 輸出 - )");
                 Thread.Sleep(900);
                 for (int i = 0; i < 20; i++)
                 {
                     Thread.Sleep(150); Console.Write("-");
                 }
-                Console.WriteLine();
+                //Console.WriteLine();
             });
             Console.WriteLine($"啟動執行兩個執行緒");
 
             thread1.Start();thread2.Start();
 
             thread1.Join();
-            Console.WriteLine($"執行緒1 執行完畢");
+            //Console.WriteLine($"執行緒1 執行完畢");
             thread2.Join();
-            Console.WriteLine($"執行緒2 執行完畢");
+            //Console.WriteLine($"執行緒2 執行完畢");
 
-            Console.WriteLine("Press any key for continuing...");
-            Console.ReadKey();
+            Console.WriteLine();
+
+            //Console.WriteLine("Press any key for continuing...");
+            //Console.ReadKey();
         }
     }
 }
