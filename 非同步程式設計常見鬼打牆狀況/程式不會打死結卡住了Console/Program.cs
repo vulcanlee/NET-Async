052ec5894b9f5665fe360624408beb2effd22d37
李進興 Vulcan <vulcan@miniasp.com>
2019-04-15 20:27:36 +0800

    然而同樣的程式碼卻在 Console, .NET Core 下卻不會發生

---
+++ 非同步程式設計常見鬼打牆狀況/程式不會打死結卡住了Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 程式不會打死結卡住了Console
+{
+    class Program
+    {
+static void Main(string[] args)
+{
+    var value = MainAsync().Result;
+    Console.WriteLine(value);
+}
+static async Task<string> MainAsync()
+{
+    var sumTask = SumAsync(168, 89);
+    var result = sumTask.Result;
+    return result;
+}
+static async Task<string> SumAsync(int a, int b)
+{
+    var client = new HttpClient();
+    var task = client.GetStringAsync(
+        "https://lobworkshop.azurewebsites.net" +
+        $"/api/RemoteSource/Add/{a}/{b}/2");
+    var result = await task;
+    return result;
+}
+    }
+}
