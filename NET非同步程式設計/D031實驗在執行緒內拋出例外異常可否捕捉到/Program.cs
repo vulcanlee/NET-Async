82208149024e9bee43d8fcbd13cacdaa11cc6561
Vulcan <vulcan.lee@gmail.com>
2020-09-25 21:13:30 +0800

    D031.實驗在執行緒內拋出例外異常可否捕捉到

---
+++ NET非同步程式設計/D031實驗在執行緒內拋出例外異常可否捕捉到/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace D031實驗在執行緒內拋出例外異常可否捕捉到
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Main Thread Id :" +
+               $"{Thread.CurrentThread.ManagedThreadId}");
+            try
+            {
+                throw new Exception("Capture Main Exception");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Thread thread = new Thread(() =>
+                {
+                    Console.WriteLine($"New Thread Id :" +
+                        $"{Thread.CurrentThread.ManagedThreadId}");
+                    Thread.Sleep(3000);
+                    throw new Exception("Oh Oh");
+                });
+                thread.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
